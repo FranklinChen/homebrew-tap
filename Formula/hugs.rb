645ef144eeafd92376fc8e090902c7469dbfc69c
Franklin Chen <franklinchen@franklinchen.com>
2015-10-15 16:17:35 -0400

    Add Hugs.

---
+++ Formula/hugs.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class Hugs < Formula
+  desc 'Hugs implementation of Haskell'
+  homepage 'http://www.haskell.org/hugs/'
+  head 'https://github.com/FranklinChen/hugs98-plus-Sep2006.git'
+
+  depends_on 'readline'
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+
+  test do
+    (testpath/'hello.hs').write <<-EOS.undent
+      import Data.List (sort)
+
+      main :: IO ()
+      main = putStrLn (sort "Hello, world!")
+    EOS
+
+    system "#{bin}/runhugs", testpath/'hello.hs'
+  end
+end
